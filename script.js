94140cfd1cb8901912784afc0250f273ea8902c0
Wolvrin12 <anouarchlih31@gmail.com>
2023-08-14 18:28:52 +0100

    Renaming javascript

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-let slides = document.getElementsByClassName("slide");
-let dots = document.getElementsByClassName("dot");
-let slideIndex = 1;
+const slides = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("dot");
+const slideIndex = 1;
 
-function plusSlides(n) {
+const moveSlide = (n) => {
   showSlides((slideIndex += n));
-}
+};
 
-function currentSlide(n) {
+const changeSlide = (n) => {
   showSlides((slideIndex = n));
-}
+};
 
-function showSlides(n) {
+const showSlides = (n) => {
   let i;
+
   if (n > slides.length) {
     slideIndex = 1;
   }
+
   if (n < 1) {
     slideIndex = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-}
+};
 
 showSlides(slideIndex);
